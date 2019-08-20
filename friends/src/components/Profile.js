6d38e99c723ef66aa0feda855330c9c9aa5a2637
Alex <agordon1123@gmail.com>
2019-08-20 18:56:57 -0400

    get request working properly

--- friends/src/components/Profile.js
+++ friends/src/components/Profile.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { axiosWithAuth } from '../utilities/axiosWithAuth';
 
-const Profile = props => {
-    console.log(props);
+const Profile = () => {
+    const [friends, setFriends] = useState({
+        friends: []
+    })
+    console.log(friends);
+
+    useEffect(() => {
+        axiosWithAuth()
+            .get('http://localhost:5000/api/friends')
+            .then(res => {
+                setFriends({ friends: res.data })
+            })
+            .catch(err => console.log(err))
+    }, [])
+
     return (
         <div>
             <h1>Hello from Profile</h1>
